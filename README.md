242cc30ba1c49a20a0d5fadb8725d7f9e2028b70
ltphat2204 <ltphat2204@gmail.com>
2024-06-29 18:23:01 +0700

    Setup project structure and add app logo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # UI Community
 
 UI Community is the platform where participants can share their snippet ui code.
+
+![Logo](/public/logo-name.png)
--- app.vue
+++ app.vue
@@ -1,4 +1,14 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'UI community',
+  ogTitle: 'UI community',
+  description: 'UI Community is the platform where participants can share their snippet ui code.',
+  ogDescription: 'UI Community is the platform where participants can share their snippet ui code.',
+})
+</script>
+
 <template>
-  <div>
-  </div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
 </template>
