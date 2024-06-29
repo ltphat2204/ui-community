141f9ebffabc261861e925fc9e67c5efe26c03fa
ltphat2204 <ltphat2204@gmail.com>
2024-06-30 01:54:35 +0700

    Change method of connecting to Firebase to hide sensitive information

--- app.vue
+++ app.vue
@@ -5,7 +5,6 @@ useSeoMeta({
   description: 'UI Community is the platform where participants can share their snippet ui code.',
   ogDescription: 'UI Community is the platform where participants can share their snippet ui code.',
 })
-
 </script>
 
 <template>
