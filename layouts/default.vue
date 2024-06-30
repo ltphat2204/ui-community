8703094604889c2c03f79a0ce7dad41a377ed4bd
ltphat2204 <ltphat2204@gmail.com>
2024-06-30 13:05:59 +0700

    fix: Fix responsive UI
    Fix responsive UI and remove the platform name in the introduction section.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-gray-200 h-screen w-screen overflow-auto">
+    <div class="bg-gray-200 h-screen w-screen overflow-auto px-4">
         <HeaderNav/>
         <main class="my-8 text-gray-700">
             <slot></slot>
--- pages/index.vue
+++ pages/index.vue
@@ -13,9 +13,8 @@ const handleSearch = async () => {
 
 <template>
     <section class="w-full max-w-[800px] mx-auto flex flex-col items-center gap-2 text-center">
-        <span class="text-xl font-black bg-gradient-to-r from-emerald-500 via-sky-500 to-blue-900 inline-block text-transparent bg-clip-text">UI Community</span>
-        <h1 class="text-6xl font-bold">UI snippets sharing platform</h1>
-        <p class="text-lg">Where you can share your UI snippets with the world.</p>
+        <h1 class="text-4xl md:text-6xl font-bold">UI snippets sharing platform</h1>
+        <p class="text-sm md:text-lg">Where you can share your UI snippets with the world.</p>
     </section>
     <section class="w-full max-w-[1100px] mx-auto mt-8">
         <form @submit.prevent="handleSearch" class="w-full max-w-[800px] mx-auto border border-gray-300 rounded-lg py-2 px-4 bg-white flex items-center">
